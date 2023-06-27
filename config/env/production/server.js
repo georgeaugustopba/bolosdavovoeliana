452eeb597e63d4c919a86738925c0fc23e8c3c52
georgeaugustopba <71203526+georgeaugustopba@users.noreply.github.com>
2023-06-27 16:09:54 -0300

    a

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,7 @@
 // Path: ./config/env/production/server.js`
 export default ({ env }) => ({
     proxy: true,
-    url: env('https://git.heroku.com/bolosdavovo.git'), // Sets the public URL of the application.
+    url: env('MY_HEROKU_URL'), // Sets the public URL of the application.
     app: {
         keys: env.array('APP_KEYS')
     },
